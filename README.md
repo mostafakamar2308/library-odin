b251f89a9ab530cf796b6dc5fe9e6b43a018a2ed
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2021-12-22 01:23:19 +0200

    before adding the completed function and Remove book functions

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # library-odin
+
 A training project for The Odin project boot camp
 
-
 ## features to do
-  
-  1. Site Skeleton
-  2. Display read books
-  3. Add new Books
-  4. Remove Books
-  5. Update Books 
 
+1. Remove Books
 
 ## Features Added
-  
 
+1.Modal for adding new Books
+2.Site Skeleton
+3.Display and update read books
--- index.js
+++ index.js
@@ -1,18 +1,83 @@
 /* Open and close the modal to add new books*/
 //Defining Constants of the modal
 const modal = document.querySelector("#add-new-book");
+const form = document.querySelector("#book-form");
+const submit = document.querySelector("#submit");
 const modalStop = document.querySelector(".close");
 const modalAppearNav = document.querySelector(".add-new-btn-nav");
 const modalAppearBody = document.querySelector(".add-new-btn-body");
+const myLibrary = document.querySelector(".books");
 //Adding the functionalities
-modalStop.addEventListener("click", function () {
+modalStop.addEventListener("click", closeModal);
+modalAppearNav.addEventListener("click", openModal);
+modalAppearBody.addEventListener("click", openModal);
+
+function closeModal() {
   modal.style.display = "none";
-});
-modalAppearNav.addEventListener("click", function () {
+}
+function openModal() {
   modal.style.display = "block";
-});
-modalAppearBody.addEventListener("click", function () {
-  modal.style.display = "block";
+}
+/*End of Open and close the modal to add new books*/
+/*Adding the Book Constructors */
+function book(bookName, author, pageNumber, completed) {
+  this.bookName = bookName;
+  this.author = author;
+  this.pageNumber = pageNumber;
+  this.completed = completed;
+}
+
+/*Make an array to store the books*/
+const myBooks = [];
+/*Take the value by the user to make new books and push it to the array*/
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  let newBookName = document.querySelector("#name").value;
+  let newBookAuthor = document.querySelector("#author").value;
+  let newBookPageNum = document.querySelector("#num").value;
+  let newBookCompleted = document.querySelector("#completed").value;
+  console.log(newBookName, newBookAuthor, newBookPageNum, newBookCompleted);
+
+  myBooks.push(
+    new book(newBookName, newBookAuthor, newBookPageNum, newBookCompleted)
+  );
+  document.querySelector("#name").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#num").value = "";
+  document.querySelector("#completed").value = "";
+  closeModal();
+  myLibrary.innerHTML = "";
+  for (let i = 0; i < myBooks.length; i++) {
+    displayBooks(myBooks[i]);
+  }
 });
 
-/*End of Open and close the modal to add new books*/
+/*Display the books in the DOM */
+function displayBooks(e) {
+  let article = document.createElement("article");
+  article.classList.add(`${e.bookName}`);
+  let name = document.createElement("h4");
+  name.innerHTML = e.bookName;
+  let author = document.createElement("h5");
+  author.innerHTML = e.author;
+  let pageNum = document.createElement("p");
+  pageNum.innerHTML = `Number of pages: <span class="number">${e.pageNumber} </span>`;
+  let completed = document.createElement("p");
+  completed.innerHTML = `Completed <span
+  class="completed"
+  style="
+  font-weight: bolder;
+  font-size: 1.2em;
+  color: white;
+  margin-left: 20px;
+  cursor: pointer;
+  "
+  >
+  &#10003;
+  </span>`;
+  article.appendChild(name);
+  article.appendChild(author);
+  article.appendChild(pageNum);
+  article.appendChild(completed);
+  myLibrary.appendChild(article);
+}
